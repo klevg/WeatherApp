196b884d66577240f1e4ed500522b1dac151f889
Евгений Клебан <kleban_eugene@tut.by>
2019-06-21 15:24:15 +0300

    initial commit

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Евгений Клебан on 6/21/19.
+//  Copyright © 2019 klevg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
